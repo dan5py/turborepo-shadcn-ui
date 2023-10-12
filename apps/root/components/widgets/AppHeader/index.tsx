5f7a8084b7f01fef2cae9eab93e5e6cbfca5f23e
YZ13 <101273441+YZ13-ENV@users.noreply.github.com>
2023-10-12 18:50:34 +0500

    added type folder

--- apps/root/components/widgets/AppHeader/index.tsx
+++ apps/root/components/widgets/AppHeader/index.tsx
@@ -13,7 +13,7 @@ const AppHeader = () => {
         <header className='flex items-center w-full h-20 py-2'>
             <div className="flex items-center justify-between w-full max-w-5xl mx-auto h-fit">
                 <Image src={dm} width={36} height={36} alt='dm-logo' />
-                <Avatar size={36} src={null} fallbackImage={empty} />
+                <Avatar size={36} src={user?.photoURL} fallbackImage={empty} />
             </div>
         </header>
     )
