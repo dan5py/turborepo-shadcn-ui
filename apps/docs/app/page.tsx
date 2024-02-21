dd06e92b807802610e185aa8acb0381886041dfb
evgenius1424 <evgenius1424@yandex.ru>
2024-02-21 12:19:27 +0100

    fix(Vite application) all issues resolved and now we can use shared components from ui package.

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@repo/ui/components/ui/button";
+import { Button } from "@repo/ui";
 
 export default function Page() {
   return (
--- apps/vite/vite.config.ts
+++ apps/vite/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@ui": path.resolve(__dirname, "../../packages/ui/src"),
+    },
+  },
+});
