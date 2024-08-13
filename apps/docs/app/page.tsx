bfc33ca3fae3d3bd97e5ed5963cfa2318caaaa11
Art Mashanin <artjom.mashanin@gmail.com>
2024-08-13 22:59:16 +0300

    feat: add vite package that can share code

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,9 +1,15 @@
 import { Button } from "@repo/ui/components/ui/button";
+import { Label } from "@repo/ui/components/ui/label";
+import { Skeleton } from "@repo/ui/components/ui/skeleton";
 
 export default function Page() {
   return (
     <main>
-      <Button>Click me</Button>
+      <div className="flex flex-col">
+        <Button>Click me</Button>
+        <Label>Label</Label>
+        <Skeleton className="w-[100px] h-[20px] rounded-full" />
+      </div>
     </main>
   );
 }
