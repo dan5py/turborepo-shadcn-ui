32b3839446de01cecc4c0808511d6a340eb20dd4
PythonPete32 <aaron@blackscale.io>
2024-10-12 18:10:58 +0000

    feat: :rocket: initial commit

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@repo/ui/components/ui/button";
 export default function Page() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="mb-4 text-2xl font-bold">Docs</h1>
+      <h1 className="mb-4 text-2xl font-bold">App</h1>
       <Button>Click me</Button>
     </main>
   );
